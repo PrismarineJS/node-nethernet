4fea617f58bc76921b54c355e580c37703fb7a3b
unknown <lucienharryholloway@hotmail.co.uk>
2025-01-30 00:27:10 +0000

    Initial implementation

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,7 +1,108 @@
 /* eslint-env mocha */
+process.env.DEBUG = '*'
+const { Server, Client } = require('node-nethernet')
 
-describe('basic', () => {
-  it('test', () => {
+async function pingTest () {
+  return new Promise((resolve, reject) => {
+    const message = 'FMCPE;JSRakNet - JS powered RakNet;408;1.16.20;0;5;0;JSRakNet;Creative;'
+    const server = new Server()
+    server.setAdvertisement(Buffer.from(message))
+    const client = new Client({ networkId: server.networkId })
 
+    client.once('pong', (packet) => {
+      console.log('PONG data', packet)
+      const msg = packet.data?.toString()
+      if (!msg || msg !== message) throw Error(`PONG mismatch ${msg} != ${message}`)
+      console.log('OK')
+      client.close()
+      server.close()
+      setTimeout(() => {
+        resolve() // allow for server + client to close
+      }, 500)
+    })
+
+    server.listen()
+    client.ping()
   })
+}
+
+async function connectTest () {
+  return new Promise((resolve, reject) => {
+    const message = 'FMCPE;JSRakNet - JS powered RakNet;408;1.16.20;0;5;0;JSRakNet;Creative;'
+    const server = new Server()
+    server.setAdvertisement(Buffer.from(message))
+    const client = new Client(server.networkId)
+
+    server.listen()
+    let lastC = 0
+    client.on('connected', () => {
+      console.log('connected!')
+      client.on('encapsulated', (encap) => {
+        console.assert(encap[0] === 0xf0)
+        const ix = encap[1]
+        if (lastC++ !== ix) {
+          throw Error(`Packet mismatch: ${lastC - 1} != ${ix}`)
+        }
+        client.send(encap)
+      })
+    })
+    let lastS = 0
+    server.on('encapsulated', (encap) => {
+      console.assert(encap[0] === 0xf0)
+      const ix = encap[1]
+      if (lastS++ !== ix) {
+        throw Error(`Packet mismatch: ${lastS - 1} != ${ix}`)
+      }
+      if (lastS === 50) {
+        client.close()
+        server.close()
+        resolve(true)
+      }
+    })
+    server.on('openConnection', (client) => {
+      console.debug('Client opened connection')
+      for (let i = 0; i < 50; i++) {
+        const buf = Buffer.alloc(1000)
+        for (let j = 0; j < 64; j += 4) buf[j] = j + i
+        buf[0] = 0xf0
+        buf[1] = i
+        client.send(buf)
+      }
+    })
+    client.connect()
+  })
+}
+
+async function kickTest () {
+  return new Promise((resolve, reject) => {
+    const server = new Server()
+    server.setAdvertisement(Buffer.from([0]))
+    const client = new Client(server.networkId)
+
+    server.on('openConnection', () => {
+      console.log('new connection')
+      client.close()
+    })
+    server.listen()
+    client.on('disconnect', packet => {
+      console.log('Client got disconnect', packet)
+      try {
+        client.send(Buffer.from('\xf0 yello'))
+      } catch (e) {
+        console.log('** Expected error 😀 **', e)
+        server.close()
+        client.close()
+        resolve()
+      }
+    })
+
+    client.connect()
+  })
+}
+
+describe('server tests', function () {
+  this.timeout(30000)
+  it('ping test', pingTest)
+  it('connection test', connectTest)
+  it('kick test', kickTest)
 })
