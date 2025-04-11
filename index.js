9b04964cd37555975821a6b53d066d3714d5c1e6
LucienHH <lucienharryholloway@hotmail.co.uk>
2025-04-11 01:22:09 +0100

    Export `SignalType`

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const { Server } = require('./src/server')
 const { ServerData } = require('./src/discovery/ServerData')
 const { SignalStructure } = require('./src/signalling')
 
+const SignalType = {
+  ConnectRequest: 'CONNECTREQUEST',
+  ConnectResponse: 'CONNECTRESPONSE',
+  CandidateAdd: 'CANDIDATEADD',
+  ConnectError: 'CONNECTERROR'
+}
+
 module.exports = {
   Client,
   Server,
   ServerData,
+  SignalType,
   SignalStructure
 }
